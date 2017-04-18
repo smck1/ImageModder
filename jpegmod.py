9ab55fa9878c75b5e49560ae398aebea77966532
smck1 <s.mckeown@napier.ac.uk>
2017-04-18 21:31:27 +0100

    fixed jpegmod image/copy/qtables references.

--- jpegmod.py
+++ jpegmod.py
@@ -20,10 +20,10 @@ def savebaselineJPEG(image, fname, outpath, optimize=True, quality=75, recompres
 
     try:
         if useexistingqtables:
-            image.save(fpath, subsample="keep", optimize=optimize, qtables="keep")
+            image.save(fpath, subsample="keep", optimize=optimize, qtables=image.quantization)
         else:
             image.save(fpath, subsample="keep", optimize=optimize, quality=quality)
-        print "Saved {}".format(fpath)
+
     except IOError, m:
         print "Baseline JPEG creation failed for: {}. \nReason:{}".format(fname,
         m)
@@ -34,8 +34,8 @@ def saverotation(image, fname, outpath, degreescounterlockwise=90):
     im = copy(image)
     im = im.rotate(degreescounterlockwise)
     try:
-        im.save(fpath, subsample="keep", qtables="keep", optimize=True)
-        print "Saved {}".format(fpath)
+        im.save(fpath, subsample="keep", qtables=image.quantization, optimize=True)
+
     except IOError, m:
         print "Rotated({}) image creation failed for: {}. \nReason:{}".format(degreescounterlockwise,fname,m)
 
@@ -46,8 +46,8 @@ def saverescale(image, fname, outpath, scalefactor=0.5):
     im = copy(image)
     im = im.resize(newsize)
     try:
-        im.save(fpath, subsample="keep", qtables="keep", optimize=True)
-        print "Saved {}".format(fpath)
+        im.save(fpath, subsample="keep", qtables=image.quantization, optimize=True)
+
     except IOError, m:
         print "Resize({}) image creation failed for: {}. \nReason:{}".format(scalefactor,fname,m)
 
@@ -57,44 +57,49 @@ def savethumb(image, fname, outpath, size=(128,128)):
     im = copy(image)
     im.thumbnail(size)
     try:
-        im.save(fpath, subsample="keep", qtables="keep", optimize=True)
-        print "Saved {}".format(fpath)
-    except IOError, m:
-        print "Resize({}) image creation failed for: {}. \nReason:{}".format(size,fname,m)
+        im.save(fpath, subsample="keep", qtables=image.quantization, optimize=True)
 
-def savecrop(image, fname, outpath, cropabsolute=None, cropfactors=(0.2, 0.2, 0.2, 0.2)):
+    except IOError, m:
+        print "Thumbnail({}) image creation failed for: {}. \nReason:{}".format(size,fname,m)
+
+def savecrop(image, fname, outpath, cropabsolute=None, cropfactors=[0.2, 0.2, 0.2, 0.2]):
     """
      Save a cropped version of the image.
      Cropfactors correspond to what portion of the original image size should be chopped off, with values corresponding to:
      left, upper, right, lower
      if cropabsolute is specified, absolute values are used instead of factors of the original's height and width.
     """
-    fpath = os.path.join(outpath, fname + '_cropped{}'.format(cropfactors) + '.jpg')
     if cropabsolute:
         if type(cropabsolute) != list:
             raise Exception("cropabsolute must be a list of length 4.")
         else:
             if len(cropabsolute) !=4:
                 raise Exception("Please provide 4 values corresponsindg to [left, upper, right, lower]")
-        cropbox =   (int(image.width + cropfactors[0]),
-                    int(image.height + cropfactors[1]),
-                    int(image.width - cropfactors[2]),
-                    int(image.height - cropfactors[3])
+        cropbox =   (int(cropabsolute[0]),
+                    int(cropabsolute[1]),
+                    int(image.width - cropabsolute[2]),
+                    int(image.height - cropabsolute[3])
                     )
-
+        fpath = os.path.join(outpath, fname + '_cropped{}'.format(cropabsolute) + '.jpg')
     else:
+        if type(cropfactors) != list:
+            raise Exception("cropfactors must be a list of length 4.")
+        else:
+            if len(cropfactors) !=4:
+                raise Exception("Please provide 4 values corresponsindg to [left, upper, right, lower]")
         cropbox =   (int(image.width * cropfactors[0]),
                     int(image.height * cropfactors[1]),
                     int(image.width * (1-cropfactors[2])),
                     int(image.height * (1-cropfactors[3]))
                     )
+        fpath = os.path.join(outpath, fname + '_cropped{}'.format(cropfactors) + '.jpg')
     im = copy(image)
     im = im.crop(cropbox)
     try:
-        im.save(fpath, subsample="keep", qtables="keep", optimize=True)
-        print "Saved {}".format(fpath)
+        im.save(fpath, subsample="keep", qtables=image.quantization, optimize=True)
+
     except IOError, m:
-        print "Crop({}) image creation failed for: {}. \nReason:{}".format(cropfactors,fname,m)
+        print "Crop({}) image creation failed for: {}. \nReason:{}".format(cropabsolute,cropfactors,fname,m)
 
 def saveflip(image, fname, outpath, axis='x'):
     """ Flip the image on the x or y axis """
@@ -107,35 +112,35 @@ def saveflip(image, fname, outpath, axis='x'):
     else:
         raise Exception('No valid axis procvided for flipping: {} - received value: {}'.format(fname, axis))
     try:
-        im.save(fpath, subsample="keep", qtables="keep", optimize=True)
-        print "Saved {}".format(fpath)
+        im.save(fpath, subsample="keep", qtables=image.quantization, optimize=True)
+
     except IOError, m:
         print "Flipped({}) image creation failed for: {}. \nReason:{}".format(axis,fname,m)
 
 def savewatermarked(image, fname, outpath, watermark):
-    im = image.copy()
     """
     Save a watermarked version of the image. resize watermark to 10% of the image height,
     with a minimum height of 40
     """
+    im = image.copy()
     fpath = os.path.join(outpath, fname + '_watermarked' + '.jpg')
     targetheight = image.height / 10
     waterwidthscaler = watermark.width / watermark.height
-    targetwdith = int(targetheight * waterwidthscaler)
+    targetwidth = int(targetheight * waterwidthscaler)
     # Enforce minimum size
     if targetheight < 40:
         targetheight = 40
-        targetwdith = int(targetheight * waterwidthscaler)
+        targetwidth = int(targetheight * waterwidthscaler)
         # It's too wide, skew  it to fit.
-        if targetwdith > image.width:
-            targetwdith = image.width
+        if targetwidth > image.width:
+            targetwidth = image.width
     # Scale the watermark
-    scaledwatermark = watermark.resize((targetwdith, targetheight))
+    scaledwatermark = watermark.resize((targetwidth, targetheight))
     # Add it to the image
-    im.paste(scaledwatermark, (image.width - targetwdith, image.height - targetheight), scaledwatermark)
+    im.paste(scaledwatermark, (im.width - targetwidth, im.height - targetheight))
     try:
-        im.save(fpath, subsample="keep", qtables="keep", optimize=True)
-        print "Saved {}".format(fpath)
+        im.save(fpath, subsample="keep", qtables=image.quantization, optimize=True)
+
     except IOError, m:
         print "Watermarked image creation failed for: {}. \nReason:{}".format(fname,m)
 
@@ -162,8 +167,8 @@ def saveenhanced(image, fname, outpath, colourfactor=1, brightnessfactor=1, cont
         im = en.enhance(sharpnessfactor)
 
     try:
-        im.save(fpath, subsample="keep",qtables="keep", optimize=True)
-        print "Saved {}".format(fpath)
+        im.save(fpath, subsample="keep",qtables=image.quantization, optimize=True)
+
     except IOError, m:
         print "Enhanced({}) image creation failed for: {}. \nReason:{}".format(estring,fname,m)
 
